8a3fb41d5cff7a2bb25b676667d7c038a680988d
Hendra Faerae <hfaerae@inspira.web.id>
2020-01-21 17:28:39 +0700

    Updated

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "test_spm",
+    name: "NavKit",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "test_spm",
-            targets: ["test_spm"]),
+            name: "NavKit",
+            targets: ["NavKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,8 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "test_spm",
-            dependencies: []),
+            name: "NavKit",
+            dependencies: [],
+            path: "Sources"
+        ),
         .testTarget(
             name: "test_spmTests",
             dependencies: ["test_spm"]),
